93b5206cd0bd3d972196df99bf4b2b98d3ca8dfa
neo <neo@neos-iMac.local>
2017-03-15 23:59:13 -0700

    Modified model. Use generators. Visualizing loss.
    Signed-off-by: neo <neo@neos-iMac.local>

--- model.py
+++ model.py
@@ -1,40 +1,92 @@
 import csv
 import cv2
 import numpy as np
+import os
 from keras.layers import Lambda
+import sklearn
+from random import shuffle
 
-lines = []
+#   lines = []
+#   with open('../project3_data/driving_log.csv') as csvfile:
+#       reader = csv.reader(csvfile)
+#       for line in reader:
+#           lines.append(line)
+
+samples = []
 with open('../project3_data/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
-        lines.append(line)
+        samples.append(line)
 
-images = []
-measurements = []
-for line in lines:
-    source_path = line [0]  # center, left, right
-    filename = source_path.split('/')[-1]
-    current_path = '../project3_data/IMG/'+filename
-    image = cv2.imread(current_path)
-    images.append(image)
-    measurement = float(line[3])
-    measurements.append(measurement)
+print("Total data:   {} samples".format(len(samples)))
 
-augmented_images, augmented_measurements = [], []
-for image, measurement in zip(images, measurements):
-    augmented_images.append(image)
-    augmented_measurements.append(measurement)
-    augmented_images.append(cv2.flip(image,1))
-    augmented_measurements.append(measurement*(-1.0))
+from sklearn.model_selection import train_test_split
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
-X_train = np.array(augmented_images)
-y_train = np.array(augmented_measurements)
+print("Training Set:   {} samples".format(len(train_samples)))
+print("Test Set:       {} samples".format(len(validation_samples)))
+
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+            
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                name = '../project3_data/IMG/'+batch_sample[0].split('/')[-1]
+                center_image = cv2.imread(name)
+                center_angle = float(batch_sample[3])
+                images.append(center_image)
+                angles.append(center_angle)
+            
+            augmented_images, augmented_angles = [], []
+            for image, angle in zip(images, angles):
+                augmented_images.append(image)
+                augmented_angles.append(angle)
+                augmented_images.append(cv2.flip(image,1))
+                augmented_angles.append(angle*(-1.0))
+            
+            # trim image to only see section with road
+            X_train = np.array(augmented_images)
+            y_train = np.array(augmented_angles)
+            yield sklearn.utils.shuffle(X_train, y_train)
+
+# compile and train the model using the generator function
+train_generator = generator(train_samples, batch_size=32)
+validation_generator = generator(validation_samples, batch_size=32)
+
+
+
+#images = []
+#measurements = []
+#for line in lines:
+#    source_path = line [0]  # center, left, right
+#    filename = source_path.split('/')[-1]
+#    current_path = '../project3_data/IMG/'+filename
+#    image = cv2.imread(current_path)
+#    images.append(image)
+#    measurement = float(line[3])
+#    measurements.append(measurement)
+#
+#augmented_images, augmented_measurements = [], []
+#for image, measurement in zip(images, measurements):
+#    augmented_images.append(image)
+#    augmented_measurements.append(measurement)
+#    augmented_images.append(cv2.flip(image,1))
+#    augmented_measurements.append(measurement*(-1.0))
+#
+#X_train = np.array(augmented_images)
+#y_train = np.array(augmented_measurements)
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 from keras.layers import Cropping2D
+import matplotlib.pyplot as plt
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
@@ -51,7 +103,25 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse',optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2,shuffle=True, nb_epoch=4)
+#history_object = model.fit(X_train, y_train, validation_split=0.2,shuffle=True, nb_epoch=4, verbose=1)
+
+history_object = model.fit_generator(train_generator,
+    samples_per_epoch = len(train_samples)*2, #data augmented
+    validation_data = validation_generator,
+    nb_val_samples = len(validation_samples),
+    nb_epoch=5, verbose=1)
+
+### print the keys contained in the history object
+print(history_object.history.keys())
+
+### plot the training and validation loss for each epoch
+plt.plot(history_object.history['loss'])
+plt.plot(history_object.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.show()
 
 model.save('model.h5')
 exit()
